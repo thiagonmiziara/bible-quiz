f0ee7d61107a1007a0ffb7e605c06ba1db1ef5dc
thiagonmiziara <thiagonmiziara@gmail.com>
2025-04-24 14:55:10 -0300

    Feat: remove server only

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import "server-only";
 
 const decodedKey = Buffer.from(
   process.env.FIREBASE_PRIVATE_KEY_BASE64 as string,
